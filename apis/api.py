7a69b6ff4b6fb88e74526d1b480ee5d02cf0eaf6
Yashwanth70 <yash.mydam@gmail.com>
2018-06-21 21:05:38 +0530

    added records

--- apis/api.py
+++ apis/api.py
@@ -21,6 +21,7 @@ def search():
     api_url = 'http://localhost:9200/my-tweets/_search'
     headers = {'Content-Type': 'application/json','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'}
     response = requests.post(api_url,data=payload, headers=headers)
+    return response.content.decode('utf-8')
     if response.status_code == 200:
         return response.content.decode('utf-8')
     else:
